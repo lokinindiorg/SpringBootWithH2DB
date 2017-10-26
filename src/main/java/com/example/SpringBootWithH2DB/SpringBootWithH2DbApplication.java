c7dd412f9d5d7ec3f1d2c37c18b8d839c46a1e9c
lokinindiorg <33128620+lokinindiorg@users.noreply.github.com>
2017-10-26 11:18:49 -0400

    Add files via upload

---
+++ src/main/java/com/example/SpringBootWithH2DB/SpringBootWithH2DbApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringBootWithH2DB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithH2DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithH2DbApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/SpringBootWithH2DB/StudentController.java
@@ -0,0 +1,32 @@
+package com.example.SpringBootWithH2DB;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.jws.soap.SOAPBinding;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class StudentController {
+	
+	
+	@Autowired
+	private StudentService studentService;
+
+	@GetMapping("/students/{studentId}/courses")
+	public List<Course> retrieveCoursesForStudent(@PathVariable String studentId) {
+		return studentService.retrieveCourses(studentId);
+	}
+}
